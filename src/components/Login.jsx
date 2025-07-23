56bfbf619fb8b62c0be555ec3d03464f545b5c64
ShubhamKumarSingh123 <singhshubhamkumar457@gmail.com>
2025-07-23 15:14:32 +0530

    Login and header build with signin singup form

---
+++ src/components/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import Header from './Header'
+import { BG_URL } from '../utils/constants'
+
+const Login = () => {
+
+
+    const [isSignInForm, setIsSignInForm] = useState(true);
+
+
+    const toggleSignInForm = () => {
+        setIsSignInForm(!isSignInForm)
+    }
+    return (
+        <div>
+            <Header />
+            <div className='absolute'>
+                <img src={BG_URL} />
+            </div>
+            <form
+                onSubmit={(e) => e.preventDefault()}
+                className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+            >
+                <h1 className="font-bold text-3xl py-4">
+                    Sign In
+                </h1>
+                {!isSignInForm && (
+                    <input
+                        type="text"
+                        placeholder="Full Name"
+                        className="p-4 my-4 w-full bg-gray-700"
+                    />
+                )}
+                <input
+                    type="text"
+                    placeholder="Email Address"
+                    className="p-4 my-4 w-full bg-gray-700"
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="p-4 my-4 w-full bg-gray-700"
+                />
+                {/* <p className="text-red-500 font-bold text-lg py-2"></p> */}
+                <button
+                    className="p-4 my-6 bg-red-700 w-full rounded-lg"
+                // onClick={handleButtonClick}
+                >
+                    {isSignInForm ? "Sign In" : "Sign Up"}
+                </button>
+                <p className="py-4 cursor-pointer"
+                    onClick={toggleSignInForm}
+                >
+                    {isSignInForm
+                        ? "New to Netflix? Sign Up Now"
+                        : "Already registered? Sign In Now."}
+                </p>
+            </form>
+        </div>
+    )
+}
+
+export default Login
