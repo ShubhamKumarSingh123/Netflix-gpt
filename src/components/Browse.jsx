12802b1dcd695b590dceb41871e7ce118dd69b5c
ShubhamKumarSingh123 <singhshubhamkumar457@gmail.com>
2025-07-24 18:16:35 +0530

    add the gpt movie suggestion using openai and display the movie using tmdb api's

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,27 +1,30 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import Header from './Header';
-import { API_OPTIONS } from '../utils/constants';
-import { addNowPlayingMovies } from '../redux/movieSlice';
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
 import MainContainer from './MainContainer';
 import SecondaryContainer from './SecondaryContainer';
 import usePopularMovies from '../hooks/usePopularMovies';
+import { useSelector } from 'react-redux';
+import GPTSearch from './GptSearch';
 
 const Browse = () => {
+    const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
     useNowPlayingMovies();
     usePopularMovies();
+
     return (
         <div>
             <Header />
-            <div className='w-screen'>
-                <MainContainer />
-                <SecondaryContainer />
-            </div>
-
+            {showGptSearch ? (
+                <GPTSearch />
+            ) : (
+                <>
+                    <MainContainer />
+                    <SecondaryContainer />
+                </>
+            )}
         </div>
-    )
+    );
 }
 
 export default Browse
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react'
-import { LOGO, USER_AVATAR } from "../utils/constants"
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants"
 import { auth } from '../utils/firebase';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../redux/userSlice';
+import { removeGptMovies, toggleGptSearchView } from '../redux/gptSlice';
+import { changeLanguage } from '../redux/configSlice';
 
 const Header = () => {
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const userData = useSelector((store) => store.user);
+    const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
     useEffect(() => {
 
     }, [userData])
@@ -22,6 +25,16 @@ const Header = () => {
 
         });
     }
+
+    const handleGptSearchClick = () => {
+        // Toggle GPT Search
+        dispatch(toggleGptSearchView());
+        dispatch(removeGptMovies());
+    };
+
+    const handleLanguageChange = (e) => {
+        dispatch(changeLanguage(e.target.value));
+    };
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -45,10 +58,29 @@ const Header = () => {
         // Unsiubscribe when component unmounts
         return () => unsubscribe();
     }, []);
+
     return (
         <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
             <img className="w-44 mx-auto md:mx-0" src={LOGO} alt='logo' />
             {userData && <div className='flex p-2'>
+                {showGptSearch && (
+                    <select
+                        className="p-2 m-2 bg-gray-900 text-white"
+                        onChange={handleLanguageChange}
+                    >
+                        {SUPPORTED_LANGUAGES.map((lang) => (
+                            <option key={lang.identifier} value={lang.identifier}>
+                                {lang.name}
+                            </option>
+                        ))}
+                    </select>
+                )}
+                <button
+                    className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+                    onClick={handleGptSearchClick}
+                >
+                    {showGptSearch ? "Homepage" : "GPT Search"}
+                </button>
                 <img className='w-12 h-12 ' alt='usericon' src={userData?.photoURL} />
                 <button onClick={handleSignOut} className='font-bold text-white cursor-pointer'>(Sign Out)</button>
             </div>}
